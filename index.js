4eda8a7f17b0422d92a1e3f20046646ea7600a29
Mikko Juusti <mikko.juusti@aalto.fi>
2021-01-10 19:22:35 +0200

    Added new things

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const nameData = require("./names.json")
 const path = require("path")
-
-function sortByUnicode(unicode) {
+app.locals.sorter = function sortByUnicode(unicode) {
     return function (a, b) {
         if (a[unicode] > b[unicode])
             return 1;
@@ -13,20 +12,34 @@ function sortByUnicode(unicode) {
         return 0;
     }
 }
-function amountByName(name) {
+app.locals.amountBN = function amountByName(nam) {
+
+    let name = ""
+    if (nam != "") {
+        name = nam[0].toUpperCase() + nam.slice(1).toLowerCase()
+    }
     if (nameData.names.map(a => a.name).includes(name)) {
+
         return nameData.names.find(a => a.name === name).amount;
     }
+    else if (name == "") { return "" }
     else return 0
 
 }
-function sumOfArray(array) {
+app.locals.sum = function sumOfArray(array) {
     return array.map(a => a.amount).reduce((a, b) => {
         return a + b;
     }, 0)
 }
-console.log(nameData.names.sort(sortByUnicode("amount")).reverse());
-console.log(nameData.names.sort(sortByUnicode("name")));
-console.log(sumOfArray(nameData.names))
-console.log(amountByName("Mikko"))
-console.log(amountByName("Jaakko"))
+
+
+app.use(express.static(path.join(__dirname, "/public")))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"))
+app.get("/", (req, res) => {
+    const search = (new URL(req.url, `http://${req.headers.host}`).search.toString().slice(6))
+    res.render("index.ejs", { names: nameData.names, search })
+})
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
